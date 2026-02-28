93062ac4a600c085a7ccd3d3492d9154085c21ce
Ravi Widnef <163608714+StopMotionEGames@users.noreply.github.com>
2026-02-28 14:49:28 -0300

    Starting rewrite: delete everything

--- src/js/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", () => {
- const loadPanel = document.querySelector("progressbarI");
- loadPanel.classList.add("hide");
- loadPanel.addEventListener("trabsitionend", () => {
-  document.body.removeChild(loadPanel);
- });
-})
